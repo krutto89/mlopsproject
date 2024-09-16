f35ab054e533d91dbc3743b78a9d5552a310d078
krutto89 <sitb01-028692022@student.mmust.ac.ke>
2024-09-16 14:30:56 +0300

    structure updated

--- template.py
+++ template.py
@@ -16,9 +16,9 @@ list_of_files = [
     "src/pipeline/training_pipeline.py",
     "src/pipeline/predict_pipeline.py",
     "src/utils/__init__.py",
-    "src/utils/utils.py"
+    "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception/exception"
+    "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.py",
@@ -41,3 +41,4 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass # create a empty file 
+        logging.info(f"creating empty file:{filename}")
